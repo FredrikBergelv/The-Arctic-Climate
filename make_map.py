18a7b74865eda854e0f8a0e7533f2fdc821c84e7
Fredrik Bergelv <fredrik.bergelv@live.se>
2026-02-28 17:58:58 +0100

    new seas

--- make_map.py
+++ make_map.py
@@ -13,9 +13,10 @@ fig = plt.figure(figsize=(6, 6))
 ax = plt.axes(projection=ccrs.NorthPolarStereo())
 
 # Land and ocean
-ax.add_feature(cfeature.LAND, facecolor="lightgray", alpha=1)
-ax.add_feature(cfeature.OCEAN, facecolor='white')
-ax.add_feature(cfeature.COASTLINE.with_scale("50m"), edgecolor='lightgray')
+
+ax.add_feature(cfeature.LAND, facecolor="beige", alpha=1)
+ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
+ax.add_feature(cfeature.COASTLINE.with_scale("50m"), edgecolor='gray')
 
 # Draw Arctic Circle
 lons = np.linspace(0, 360, 1000)
@@ -31,7 +32,7 @@ gl = ax.gridlines(draw_labels=False, linewidth=0.5, color='black', alpha=0.5, li
 
 # --- Locations from text with approximate lat/lon ---
 locations = {
-    "Arctic Ocean": (90, 0),
+    #"Arctic Ocean": (90, 0),
     "Greenland Sea": (75, -10),
     "Labrador Sea": (61, -55),
     "Baffin Bay": (70, -65),
@@ -39,30 +40,46 @@ locations = {
     "Canadian Archipelago": (75, -100),
     "Barents Sea": (75, 40),
     "Fram Strait": (79, -10),
-}
+    "Beaufort Sea": (73, -145),
+    "Kara Sea": (75, 70),
+    "Nordenskiöld Sea": (75, 125),
+    "East Siberian Sea": (72, 170),
+    "Norwegian Sea": (68, 5),
+    }
 
 # Plot blue dots and names
 for name, (lat, lon) in locations.items():
     print(name)
+    ax.plot(lon, lat, 'o', color='blue', markersize=5, transform=ccrs.PlateCarree())
+
     if name == "Bering Strait":
-        ax.plot(lon, lat, 'o', color='blue', markersize=5, transform=ccrs.PlateCarree())
-        ax.text(lon, lat+3, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
+        ax.text(lon, lat+2.4, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
     
     elif name == "Greenland Sea":
-        ax.plot(lon, lat, 'o', color='blue', markersize=5, transform=ccrs.PlateCarree())
         ax.text(lon, lat-3, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
   
     elif name == "Barents Sea":
-        ax.plot(lon, lat, 'o', color='blue', markersize=5, transform=ccrs.PlateCarree())
         ax.text(lon, lat-1, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
     
     elif name == "Arctic Ocean":
-        ax.plot(lon, lat, 'o', color='blue', markersize=5, transform=ccrs.PlateCarree())
         ax.text(lon, lat-3, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
-      
+        
+    elif name == "Norwegian Sea":
+         ax.text(lon, lat-2.2, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
+    
+    elif name == "Nordenskiöld Sea":
+         ax.text(lon+38, lat+5, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
+  
+    elif name == "Kara Sea":
+       ax.text(lon, lat+2, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
+    
+    elif name == "East Siberian Sea":
+       ax.text(lon-2, lat, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
+     
+       
     else:
-        ax.plot(lon, lat, 'o', color='blue', markersize=5, transform=ccrs.PlateCarree())
         ax.text(lon, lat+1, name, color='darkblue', fontsize=16, transform=ccrs.PlateCarree())
+    
 
 # Title
 plt.title("Map of the Arctic", fontsize=16)
